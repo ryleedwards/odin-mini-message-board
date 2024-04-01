afc81c223f3cae277aa45ca837ead86107d66b72
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-04-01 10:13:13 -0500

    Add form template renderin get route

--- routes/new.js
+++ routes/new.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('New Form Page');
+  res.render('form');
 });
+
+router.post('/', (req, res, next) => {});
 
 module.exports = router;
